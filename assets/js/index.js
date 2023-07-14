e9225799664bbf1cbab149a4862b45cc091cc770
Leonardo Santos Custodio <santos-contato@hotmail.com.br>
2023-07-14 14:51:30 -0300

    alteração de correção

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,5 @@
 window.onload = async () => {
-  // Obtém o elemento de vídeo da câmera
-  const cam = document.getElementById("cam");
-
-  // Função para iniciar o vídeo da câmera frontal...dispositivo notebook ou celular
+  // Função para iniciar o vídeo da câmera frontal
   const startVideo = async () => {
     try {
       const isMobileDevice =
@@ -24,11 +21,13 @@ window.onload = async () => {
         };
       }
 
+      const videoElement = document.getElementById("cam");
+
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      cam.srcObject = stream;
+      videoElement.srcObject = stream;
 
       return new Promise((resolve) => {
-        cam.onloadedmetadata = () => {
+        videoElement.onloadedmetadata = () => {
           resolve();
         };
       });
@@ -40,21 +39,11 @@ window.onload = async () => {
   // Função para carregar os modelos
   const loadModels = async () => {
     try {
-      await faceapi.nets.tinyFaceDetector.loadFromUri(
-        "/assets/lib/face-api/models"
-      );
-      await faceapi.nets.faceLandmark68Net.loadFromUri(
-        "/assets/lib/face-api/models"
-      );
-      await faceapi.nets.faceRecognitionNet.loadFromUri(
-        "/assets/lib/face-api/models"
-      );
-      await faceapi.nets.faceExpressionNet.loadFromUri(
-        "/assets/lib/face-api/models"
-      );
-      await faceapi.nets.ssdMobilenetv1.loadFromUri(
-        "/assets/lib/face-api/models"
-      );
+      await faceapi.loadTinyFaceDetectorModel("/assets/lib/face-api/models");
+      await faceapi.loadFaceLandmark68Model("/assets/lib/face-api/models");
+      await faceapi.loadFaceRecognitionModel("/assets/lib/face-api/models");
+      await faceapi.loadFaceExpressionModel("/assets/lib/face-api/models");
+      await faceapi.loadSsdMobilenetv1Model("/assets/lib/face-api/models");
     } catch (error) {
       console.error("Erro ao carregar os modelos:", error);
     }
@@ -62,47 +51,6 @@ window.onload = async () => {
 
   // Função para detectar rostos e realizar o reconhecimento facial
   const detectFaces = async () => {
-    try {
-      const labeledDescriptors = await loadLabels();
-
-      const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.6);
-
-      setInterval(async () => {
-        const detections = await faceapi
-          .detectAllFaces(cam, new faceapi.TinyFaceDetectorOptions())
-          .withFaceLandmarks()
-          .withFaceDescriptors();
-
-        const results = detections.map((d) =>
-          faceMatcher.findBestMatch(d.descriptor)
-        );
-
-        const canvas = faceapi.createCanvasFromMedia(cam);
-        faceapi.matchDimensions(canvas, cam);
-        document.body.appendChild(canvas);
-
-        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-
-        faceapi.draw.drawDetections(canvas, detections);
-        results.forEach((result, index) => {
-          const { label, distance } = result;
-          const box = detections[index].detection.box;
-
-          const drawOptions = {
-            label: `${label}`,
-          };
-
-          const drawBox = new faceapi.draw.DrawBox(box, drawOptions);
-          drawBox.draw(canvas);
-        });
-      }, 250);
-    } catch (error) {
-      console.error("Erro durante a detecção de rostos:", error);
-    }
-  };
-
-  // Função para carregar os rótulos das imagens de cada pessoa cadastrada
-  const loadLabels = async () => {
     try {
       const labels = [
         "Antony",
@@ -117,48 +65,86 @@ window.onload = async () => {
         "Rogerio",
         "Salvan",
         "Samuel",
-      ]; // Array de pessoas
-      const labeledDescriptors = [];
+      ];
 
-      for (const label of labels) {
-        const descriptions = [];
+      const labeledDescriptors = await Promise.all(
+        labels.map(async (label) => {
+          const descriptions = [];
 
-        for (let i = 1; i <= 3; i++) {
-          try {
-            const img = await faceapi.fetchImage(
-              `/assets/lib/face-api/labels/${label}/${i}.jpg`
-            );
+          for (let i = 1; i <= 3; i++) {
+            try {
+              const img = await faceapi.fetchImage(
+                `/assets/lib/face-api/labels/${label}/${i}.jpg`
+              );
 
-            const detections = await faceapi
-              .detectSingleFace(img)
-              .withFaceLandmarks()
-              .withFaceDescriptor();
+              const detections = await faceapi
+                .detectSingleFace(img)
+                .withFaceLandmarks()
+                .withFaceDescriptor();
 
-            if (detections) {
-              descriptions.push(detections.descriptor);
+              if (detections) {
+                descriptions.push(detections.descriptor);
+              }
+              console.log(label + ": Carregada foto: " + i);
+            } catch (error) {
+              console.error(
+                "Erro ao carregar a imagem de: " + label + " na posição: " + i,
+                error
+              );
             }
-            console.log(label + ": Carregada foto: " + i);
-          } catch (error) {
-            console.error(
-              "Erro ao carregar a imagem de: " + label + " na posição: " + i,
-              error
-            );
           }
-        }
 
-        if (descriptions.length > 0) {
-          const labeledDescriptor = new faceapi.LabeledFaceDescriptors(
-            label,
-            descriptions
-          );
-          labeledDescriptors.push(labeledDescriptor);
-        }
-      }
+          if (descriptions.length > 0) {
+            return new faceapi.LabeledFaceDescriptors(label, descriptions);
+          }
+        })
+      );
 
-      return labeledDescriptors;
+      const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.6);
+
+      const videoElement = document.getElementById("cam");
+      const canvas = faceapi.createCanvasFromMedia(videoElement);
+      document.body.appendChild(canvas);
+
+      const displaySize = {
+        width: videoElement.offsetWidth,
+        height: videoElement.offsetHeight,
+      };
+      faceapi.matchDimensions(canvas, displaySize);
+
+      setInterval(async () => {
+        const detections = await faceapi
+          .detectAllFaces(videoElement, new faceapi.TinyFaceDetectorOptions())
+          .withFaceLandmarks()
+          .withFaceDescriptors();
+
+        const resizedDetections = faceapi.resizeResults(
+          detections,
+          displaySize
+        );
+
+        const results = resizedDetections.map((d) =>
+          faceMatcher.findBestMatch(d.descriptor)
+        );
+
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        results.forEach((result, index) => {
+          const { label, distance } = result;
+          const box = resizedDetections[index].detection.box;
+
+          const text = `${label} (${Math.round(distance * 100) / 100})`;
+          const drawOptions = {
+            label: text,
+            lineWidth: 2,
+          };
+          const drawBox = new faceapi.draw.DrawBox(box, drawOptions);
+          drawBox.draw(canvas);
+        });
+      }, 250);
     } catch (error) {
-      console.error("Erro ao carregar os rótulos das imagens:", error);
-      return [];
+      console.error("Erro durante a detecção de rostos:", error);
     }
   };
 
